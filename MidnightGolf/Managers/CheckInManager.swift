008b190da43ca022ab7f36709c62328d6443b833
HKhafajii <147748541+HKhafajii@users.noreply.github.com>
2025-01-24 11:19:45 -0500

    Clean up

--- MidnightGolf/Managers/CheckInManager.swift
+++ MidnightGolf/Managers/CheckInManager.swift
@@ -106,22 +106,4 @@ class CheckInManager: ObservableObject {
         
     }
     
-    
-    
-    // When someone scans a qr code, it checks the user whos qr code it was in
-    
-    
-    
-    // Changes a bool that starts a timer to keep track of the time
-    
-    // OR
-    
-    // Takes the time they check in and takes the time they checkout and subtracts it from eachother to get the total amount of time clocked in
-    
-    // Checks if they're late or not
-    
-    // Checks if they clock out early
-    
-    
-    
 }
