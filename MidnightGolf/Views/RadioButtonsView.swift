4965708d0c87d093333a41c0fafba53b69ea85da
HKhafajii <147748541+HKhafajii@users.noreply.github.com>
2025-02-25 14:16:34 -0500

    fixing radio buttons

--- MidnightGolf/Views/RadioButtonsView.swift
+++ MidnightGolf/Views/RadioButtonsView.swift
@@ -1,28 +1,22 @@
-//
-//  RadioButtonsView.swift
-//  MidnightGolf
-//
-//  Created by Hassan Alkhafaji on 2/25/25.
-//
-
 import SwiftUI
+
 struct Gender: Identifiable {
     let id = UUID()
     let genderType: String
 }
 
 struct RadioButtonsView: View {
-    @State var genders: [Gender] = [
+    @Binding var isMale: Bool
+    
+    private let genders: [Gender] = [
         Gender(genderType: "Male"),
         Gender(genderType: "Female")
     ]
-
-    @State var gendersSelected: Gender?
-
+    
     var body: some View {
         HStack {
             ForEach(genders) { gender in
-                RadioButtons(gender: gender, seasonSelected: $gendersSelected)
+                RadioButtons(gender: gender, isMale: $isMale)
             }
         }
     }
@@ -30,41 +24,45 @@ struct RadioButtonsView: View {
 
 struct RadioButtons: View {
     let gender: Gender
-    @Binding var seasonSelected: Gender?
-
+    @Binding var isMale: Bool
+    
     let brandColor: Color = Color(red: 28/255, green: 35/255, blue: 40/255)
-
-    var isPressed: Bool {
-        if let seasonSelected {
-            if seasonSelected.id == gender.id { return true }
-        }
-        return false
+    
+    var isSelected: Bool {
+        gender.genderType == "Male" ? isMale : !isMale
     }
-
+    
     var body: some View {
         Button {
-            seasonSelected = gender
+         
+            if gender.genderType == "Male" {
+                isMale = true
+            } else {
+                isMale = false
+            }
         } label: {
-            Text("\(gender.genderType)")
-        }
-        .font(.title2)
-        .frame(width: 100, height: 50)
-        .padding(2)
-        .background(RoundedRectangle(cornerRadius: 8)
-            .fill(brandColor)
-        )
-        .foregroundStyle(isPressed ? .orange : .white)
-        .overlay {
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(isPressed ? Color.orange : Color.white, lineWidth: 3)
+            Text(gender.genderType)
+                .font(.title2)
+                .frame(width: 100, height: 50)
+                .padding(2)
+                .background(RoundedRectangle(cornerRadius: 8).fill(brandColor))
+                .foregroundStyle(isSelected ? .orange : .white)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(isSelected ? Color.orange : Color.white, lineWidth: 3)
+                }
         }
     }
 }
 
-#Preview(body: {
-    RadioButtonsView()
-})
-
-
-
-
+struct RadioButtonsView_Previews: PreviewProvider {
+    struct Container: View {
+        @State var isMale: Bool = true
+        var body: some View {
+            RadioButtonsView(isMale: $isMale)
+        }
+    }
+    static var previews: some View {
+        Container()
+    }
+}
